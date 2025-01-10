857f71de30d82c7de37e344119947a21d7608c86
gabrow <99gabor@gmail.com>
2025-01-10 16:38:48 +0100

    Testing different number

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "func.h"
 
 int main() {
-    int number = 5;
+    int number = 6;
     int result = factorial(number);
     std::cout << "Factorial of " << number << " is " << result << std::endl;
     return 0;
